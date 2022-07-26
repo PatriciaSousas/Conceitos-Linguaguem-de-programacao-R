8d348a2b25f95aea85937761cac0fd8b9cd92fed
Patricia Sousa <81525412+PatriciaSousas@users.noreply.github.com>
2022-07-26 15:42:49 -0300

    Add files via upload

---
+++ Funcoes + operadores matematicos com R.R
@@ -0,0 +1,60 @@
+#fUNÇÕES BASICAS EM r
+
+a <- "Heitor"
+b <- "Joao"
+
+c <- c(a,b)
+
+
+c <- c('Heitor123', 'Joao')
+c
+
+a <- c(10,3,4,2,4,2)
+a
+
+summary(a)
+
+install.packages("stringr")
+library(stringr)
+
+nome <- 'Patricia'
+sobrenome <- 'Sousa'
+
+Nomecompleto <- str_c(nome, sobrenome)
+Nomecompleto
+
+------------------------------------------------------------------
+  
+  #OPERAÇÕES MATEMATICAS 
+  
+  5+5 #soma 
+
+  10-8 #subtração 
+  
+  9/3 #divisao 
+  
+  4=2 #multiplicacao
+   
+  4** 2 #Potencia 
+  
+  5==6 #Igual?
+  
+  5! 8 #diferente?
+  
+  7<9 #Maior?
+  
+  3<2 #Menor?
+  
+  4>=5 #Maior ou igual?
+  
+  6<=8 # Menor ou igual?
+  
+  6==6 & 7==8 #E NA LOGICA DE PROGRAMACAO
+  
+  6==6 | 7==8 #OU NA LOGICA DE PROGRAMACAO
+  
+  !6 == 6 #NEGACAO 
+  
+  
+
+
